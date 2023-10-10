6f02484fe85d911993a02fa2583596db5684f0da
Ismael Carrion <iecarrion@enalsa.net>
2023-10-09 21:16:14 -0600

    Add files via upload

---
+++ getData.jsx
@@ -0,0 +1,161 @@
+const Pagination = ({ items, pageSize, onPageChange }) => {
+  const { Button } = ReactBootstrap;
+  if (items.length <= 1) return null;
+
+  let num = Math.ceil(items.length / pageSize);
+  let pages = range(1, num + 1);
+  const list = pages.map(page => {
+    return (
+      <Button variant = "primary" size="lg" key={page} onClick={onPageChange} className="page-item">
+        {page}
+      </Button>
+    );
+  });
+  return (
+    <nav>
+      <ul className="pagination">{list}</ul>
+    </nav>
+  );
+};
+const range = (start, end) => {
+  return Array(end - start + 1)
+    .fill(0)
+    .map((item, i) => start + i);
+};
+function paginate(items, pageNumber, pageSize) {
+  const start = (pageNumber - 1) * pageSize;
+  let page = items.slice(start, start + pageSize);
+  return page;
+}
+const useDataApi = (initialUrl, initialData) => {
+  const { useState, useEffect, useReducer } = React;
+  const [url, setUrl] = useState(initialUrl);
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData
+  });
+
+  useEffect(() => {
+    let didCancel = false;
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_INIT" });
+      try {
+        const result = await axios(url);
+        if (!didCancel) {
+          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+        }
+      } catch (error) {
+        if (!didCancel) {
+          dispatch({ type: "FETCH_FAILURE" });
+        }
+      }
+    };
+    fetchData();
+    return () => {
+      didCancel = true;
+    };
+  }, [url]);
+  return [state, setUrl];
+};
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      };
+    case "FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      };
+    default:
+      throw new Error();
+  }
+};
+// App that gets data from Hacker News url
+function App() {
+  const { Button } = ReactBootstrap;
+  const { Card } = ReactBootstrap;
+  const { Fragment, useState, useEffect, useReducer } = React;
+  const [query, setQuery] = useState("Harvard");
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    "https://hn.algolia.com/api/v1/search?query=Harvard",
+    {
+      hits: []
+    }
+  );
+  const handlePageChange = e => {
+    setCurrentPage(Number(e.target.textContent));
+  };
+  let page = data.hits;
+  if (page.length >= 1) {
+    page = paginate(page, currentPage, pageSize);
+    console.log(`currentPage: ${currentPage}`);
+  }
+  return (
+    <Fragment>
+      <form
+        onSubmit={event => {
+          doFetch("http://hn.algolia.com/api/v1/search?query=${query}");
+          event.preventDefault();
+        }}
+      >
+        <input 
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <Button type="submit">Search</Button>
+      </form>
+
+      <Pagination
+        items={data.hits}
+        pageSize={pageSize}
+        onPageChange={handlePageChange}
+      ></Pagination>
+      
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {page.map(item => (
+            <Card style={{ width: '36rem' }}>
+            <Card.Body>
+              <Card.Title>{item.title}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Author: {item.author} </Card.Subtitle>
+              <Card.Text>
+                {item.created_at}
+              </Card.Text>
+              <Card.Link href="#">{item.url}</Card.Link>
+            </Card.Body>
+          </Card>
+          ))}
+        </ul>
+      )}
+      <Pagination
+        items={data.hits}
+        pageSize={pageSize}
+        onPageChange={handlePageChange}
+      ></Pagination>
+    </Fragment>
+  );
+}
+
+// ========================================
+ReactDOM.render(<App />, document.getElementById("root"));
